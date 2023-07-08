45bd7958453889716cb36b08ae4c73a2cfd92926
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-07-08 05:41:46 +0300

    fixed warehouseorderform submit error

--- src/pages/warehouseOrders/components/WarehouseOrderForm.jsx
+++ src/pages/warehouseOrders/components/WarehouseOrderForm.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Formik, Form, Field, FieldArray } from 'formik';
+import { useFormik } from 'formik';
 import { TextField, Button, Box, Grid } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import { fieldToTextField } from 'formik-material-ui';
 
 const initialValues = {
-  warehouseOrder: {
     customer_id : '',
     customer_address: '',
     documentNo: '',
@@ -21,141 +19,128 @@ const initialValues = {
         quantity: 0,
       },
     ],
-  },
 };
 
-const WarehouseOrderForm = () => (
-  <div>
-    <h1>Warehouse Order Form</h1>
-    <h3>Warehouse Order header</h3>
+const WarehouseOrderForm = () => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-    <Formik
-      initialValues={initialValues}
-      onSubmit={values => {
-        console.log(values);
-      }}
-    >
-      {({ values, setFieldValue }) => (
-        <Form>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
-              <Field
-                name="warehouseOrder.documentNo"
-                label="Document No"
-                component={TextField}
-                fullWidth
-                size="small"
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <Field name="warehouseOrder.date" label="Date" component={TextField} fullWidth
-              size="small" />
-            </Grid>
-            <Grid item xs={3}>
-              <Field name="warehouseOrder.customer_id" label="Customer_id" component={TextField} fullWidth
-              size="small" />
-            </Grid>
-            <Grid item xs={3}>
-              <Field name="warehouseOrder.customer_address" label="Customer Adress" component={TextField} fullWidth
-              size="small" />
-            </Grid>
-            <Grid item xs={3}>
-              <Field
-                name="warehouseOrder.purchaseOrderNo"
-                label="Purchase Order No"
-                component={TextField}
-                fullWidth
-                size="small"
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <Field
-                name="warehouseOrder.deliveryDate"
-                label="Delivery Date"
-                component={TextField}
-                fullWidth
-                size="small"
-              />
-            </Grid>
+  const handleAddItem = () => {
+    formik.setFieldValue('items', [...formik.values.items, ...initialValues.items]);
+  };
+
+  const handleRemoveItem = index => {
+    const newItems = [...formik.values.items];
+    newItems.splice(index, 1);
+    formik.setFieldValue('items', newItems);
+  };
+
+  return (
+    <div>
+      <h1>Warehouse Order Form</h1>
+      <h3>Warehouse Order header</h3>
+
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <TextField name="customer_id" label="Customer_id" fullWidth size="small" value={formik.values.customer_id} onChange={formik.handleChange} />
           </Grid>
+          <Grid item xs={3}>
+            <TextField name="customer_address" label="Customer Adress" fullWidth size="small" value={formik.values.customer_address} onChange={formik.handleChange} />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField name="documentNo" label="Document No" fullWidth size="small" value={formik.values.documentNo} onChange={formik.handleChange} />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField name="date" label="Date" fullWidth size="small" value={formik.values.date} onChange={formik.handleChange} />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField name="purchaseOrderNo" label="Purchase Order No" fullWidth size="small" value={formik.values.purchaseOrderNo} onChange={formik.handleChange} />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField name="deliveryDate" label="Delivery Date" fullWidth size="small" value={formik.values.deliveryDate} onChange={formik.handleChange} />
+          </Grid>
+        </Grid>
 
-          <h3>Warehouse Order Items</h3>
-          <FieldArray name="warehouseOrder.items" >
-            {({ push, remove }) => (
-              <>
-                {values.warehouseOrder.items.map((_, index) => (
-                  <Box key={index}>
-                    <Box display="flex">
-                      <Box marginBottom={2} marginRight={2}>
-                        <Field
-                          name={`warehouseOrder.items.${index}.position`}
-                          label="Position"
-                          type="number"
-                          component={TextField}
-                          fullWidth
-                          size="small"
-                        />
-                      </Box>
-                      <Box marginBottom={2} marginRight={2}>
-                        <Field
-                          name={`warehouseOrder.items.${index}.itemNo`}
-                          label="Item No"
-                          type="number"
-                          component={TextField}
-                          fullWidth
-                          size="small"
-                        />
-                      </Box>
-                      <Box marginBottom={2} marginRight={2}>
-                        <Field
-                          name={`warehouseOrder.items.${index}.description`}
-                          label="Description"
-                          component={TextField}
-                          fullWidth
-                          size="small"
-                        />
-                      </Box>
-                      <Box marginBottom={2} marginRight={2}>
-                        <Field
-                          name={`warehouseOrder.items.${index}.supplierItemNo`}
-                          label="Supplier Item No"
-                          component={TextField}
-                          fullWidth
-                          size="small"
-                        />
-                      </Box>
-                      <Box marginBottom={2} marginRight={2}>
-                        <Field
-                          name={`warehouseOrder.items.${index}.quantity`}
-                          label="Quantity"
-                          type="number"
-                          component={TextField}
-                          fullWidth
-                          size="small"
-                          />
-                        </Box>
-  
-                        <Button variant="contained" onClick={() => remove(index)} style={{marginBottom: "1rem"}}>
-                          Remove
-                        </Button>
-                      </Box>
-                    </Box>
-                  ))}
-                  <Button variant="contained" onClick={() => push(initialValues.warehouseOrder.items[0])} style={{margin: "0.5rem 1rem 0.5rem 0"}} >
-                    Add Item
-                  </Button>
-                </>
-              )}
-            </FieldArray>
-  
-            <Button variant="contained" type="submit" style={{margin: "0.5rem 1rem 0.5rem 0"}}>
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
+        <h3>Warehouse Order Items</h3>
+        {formik.values.items.map((_, index) => (
+          <Box key={index}>
+            <Box display="flex">
+              <Box marginBottom={2} marginRight={2}>
+                <TextField
+                  name={`items.${index}.position`}
+                  label="Position"
+                  type="number"
+                  fullWidth
+                  size="small"
+                  value={formik.values.items[index].position}
+                  onChange={formik.handleChange}
+                />
+              </Box>
+              <Box marginBottom={2} marginRight={2}>
+                <TextField
+                  name={`items.${index}.itemNo`}
+                  label="Item No"
+                  type="number"
+                  fullWidth
+                  size="small"
+                  value={formik.values.items[index].itemNo}
+                  onChange={formik.handleChange}
+                />
+              </Box>
+              <Box marginBottom={2} marginRight={2}>
+                <TextField
+                  name={`items.${index}.description`}
+                  label="Description"
+                  fullWidth
+                  size="small"
+                  value={formik.values.items[index].description}
+                  onChange={formik.handleChange}
+                />
+              </Box>
+              <Box marginBottom={2} marginRight={2}>
+                <TextField
+                  name={`items.${index}.supplierItemNo`}
+                  label="Supplier Item No"
+                  fullWidth
+                  size="small"
+                  value={formik.values.items[index].supplierItemNo}
+                  onChange={formik.handleChange}
+                />
+              </Box>
+              <Box marginBottom={2} marginRight={2}>
+                <TextField
+                  name={`items.${index}.quantity`}
+                  label="Quantity"
+                  type="number"
+                  fullWidth
+                  size="small"
+                  value={formik.values.items[index].quantity}
+                  onChange={formik.handleChange}
+                />
+              </Box>
+
+              <Button variant="contained" onClick={() => handleRemoveItem(index)} style={{marginBottom: "1rem"}}>
+                Remove
+              </Button>
+            </Box>
+          </Box>
+        ))}
+        <Button variant="contained" onClick={handleAddItem} style={{margin: "0.5rem 1rem 0.5rem 0"}} >
+          Add Item
+        </Button>
+
+        <Button variant="contained" type="submit" style={{margin: "0.5rem 1rem 0.5rem 0"}}>
+          Submit
+        </Button>
+      </form>
     </div>
   );
-  
-  export default WarehouseOrderForm;
-  
+};
+
+export default WarehouseOrderForm;
+
